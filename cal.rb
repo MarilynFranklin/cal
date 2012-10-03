9b8890bb9b4b7fbaa3a9809ced5084817c1136b6
MarilynFranklin <marilyn.j.franklin@gmail.com>
2012-10-03 18:08:27 -0500

    Refactored generate_weeks_array

--- cal.rb
+++ cal.rb
@@ -1,4 +1,21 @@
-require './lib/calendar'
+require './lib/month'
+require './lib/year'
+
+def reassign_month_name_to_integer_value(month_string, year)
+  month_int = nil
+  MONTHS_ABBR.keys.each do |month|
+    if month_string =~ /(#{Regexp.quote(month)})/
+      month_int = MONTHS_ABBR[$1]
+    end
+  end
+  if month_int.nil?
+    puts "#{month_string} is not a valid month."
+  else
+    cal = Month.new(month_int, year)
+    cal.print_month
+  end
+end
+
 
 if ARGV.length == 2
   month_string = ARGV[0].downcase
@@ -18,7 +35,8 @@ if ARGV.length == 2
   elsif year < 1800 || year > 3000
   	puts "Sorry, cal only works for dates from year 1800 through year 3000."
   else
-    puts generate_calendar_for(month, year)
+    cal = Month.new(month, year)
+    puts cal.print_month
   end
 elsif ARGV.length == 1
   year_string = ARGV[0]
@@ -28,7 +46,8 @@ elsif ARGV.length == 1
   elsif year < 1800 || year > 3000
   	puts "Sorry, cal only works for dates from year 1800 through year 3000."
   else
-    puts generate_calendar_full_year(year)
+    cal = Year.new(year)
+    puts cal.print_year
   end
 end
 
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
-require 'calendar'
+require 'month'
+require 'year'
 
 class CalTest < Test::Unit::TestCase
 
@@ -17,67 +18,78 @@ class CalTest < Test::Unit::TestCase
   end
 
   def test_04_month_start_day
-    assert_equal 0, get_month_start_day(12, 2012)
+    cal = Month.new(12, 2012)
+    assert_equal 0, cal.month_start_day
   end
   def test_04b_month_start_day
-    assert_equal 0, get_month_start_day(1, 2000)
+    cal = Month.new(1, 2000)
+    assert_equal 0, cal.month_start_day
   end
 
   def test_04c_month_start_day
-    assert_equal 0, get_month_start_day(9, 2012)
+    cal = Month.new(9, 2012)
+    assert_equal 0, cal.month_start_day
   end  
 
   def test_04d_month_start_day
-    assert_equal 4, get_month_start_day(2, 2012)
+    cal = Month.new(2, 2012)
+    assert_equal 4, cal.month_start_day
   end 
 
   def test_04e_month_start_day
-    assert_equal 4, get_month_start_day(8, 2012)
+    cal = Month.new(8, 2012)
+    assert_equal 4, cal.month_start_day
   end
 
   def test_04f_month_start_day
-    assert_equal 0, get_month_start_day(5, 1999)
+    cal = Month.new(5, 1999)
+    assert_equal 0, cal.month_start_day
   end
   def test_05_leap_year
-    assert_equal true, leap_year?(2012)
+    cal = Month.new(5, 2012)
+    assert_equal true, cal.leap_year?
   end
 
   def test_05b_leap_year
-    assert_equal true, leap_year?(2000)
+    cal = Month.new(2, 2000)
+    assert_equal true, cal.leap_year?
   end
 
   def test_05c_leap_year
-    assert_equal false, leap_year?(1900)
+    cal = Month.new(4, 1900)
+    assert_equal false, cal.leap_year?
   end
 
   def test_05d_leap_year
-    assert_equal false, leap_year?(1993)
+    cal = Month.new(2, 1993)
+    assert_equal false, cal.leap_year?
   end
 
   def test_07_number_of_days_in_month
     assert_equal 31, MONTHS[12][:days_in_month]
   end
 
-  def test_10_generate_month_has_correct_spaces_before_and_after
-    assert_equal [" ", " ", " ", " ", "1", "2", "3",
+  def test_10_days_and_spaces_array_has_correct_spaces_before_and_after
+    cal = Month.new(5, 1993)
+    assert_equal [" ", " ", " ", " ", " ", " ", "1", "2", "3",
                  "4", "5", "6", "7", "8", "9", "10", 
                  "11", "12", "13", "14", "15", "16",
                  "17", "18", "19", "20", "21", "22", 
                  "23", "24", "25", "26", "27", "28", 
-                 "29", "30", "31", " ", " ", " ", " ", 
-                 " ", " ", " "], generate_month(31, 5)
+                 "29", "30", "31", " ", " ", " ", " ", " "], cal.days_and_spaces_array
   end
-  def test_11_month_pretty_contains_lines_for_days_in_month
-    month = generate_month(31, 5)
+  def test_11_weeks_array_is_correct
+    cal = Month.new(6, 2000)
     assert_equal ["             1  2  3  ",
                   " 4  5  6  7  8  9 10  ",
                   "11 12 13 14 15 16 17  ",
                   "18 19 20 21 22 23 24  ",
-                  "25 26 27 28 29 30 31  ",
-                  "                      "], month_pretty(month)
+                  "25 26 27 28 29 30     ",
+                  "                      "], cal.generate_weeks_array
   end
 
   def test_14_calendar_month_array_correct_num_days
+    cal = Month.new(3, 1999)
     assert_equal  """     March 1999       
 Su Mo Tu We Th Fr Sa  
     1  2  3  4  5  6  
@@ -86,9 +98,10 @@ Su Mo Tu We Th Fr Sa
 21 22 23 24 25 26 27  
 28 29 30 31           
                       
-""", generate_calendar_for(3, 1999)
+""", cal.print_month
   end
   def test_15_calendar_month_array_correct_num_days
+    cal = Year.new(1886)
     assert_equal  ["        July          ",
                    "Su Mo Tu We Th Fr Sa  ",
                    "             1  2  3  ",
@@ -96,9 +109,10 @@ Su Mo Tu We Th Fr Sa
                    "11 12 13 14 15 16 17  ",
                    "18 19 20 21 22 23 24  ",
                    "25 26 27 28 29 30 31  ",
-                   "                      "], generate_month_without_year_in_title(7, 1886)
+                   "                      "], cal.month_array(7)
   end
   def test_16_complete_year_returns_array_of_month_arrays
+    cal = Year.new(1886)
     assert_equal  [["      January         ",
                     "Su Mo Tu We Th Fr Sa  ",
                     "                1  2  ",
@@ -194,10 +208,11 @@ Su Mo Tu We Th Fr Sa
                     "12 13 14 15 16 17 18  ",
                     "19 20 21 22 23 24 25  ",
                     "26 27 28 29 30 31     ",
-                    "                      "]], complete_year(1886)
+                    "                      "]], cal.complete_year
   end
 
-  def test_17_pretty_year_is_correct
+  def test_17_three_column_array_is_correct
+    cal = Year.new(1886)
     assert_equal  ["      January               February               March          ",
                    "Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  ",
                    "                1  2      1  2  3  4  5  6      1  2  3  4  5  6  ",
@@ -229,9 +244,10 @@ Su Mo Tu We Th Fr Sa
                    "10 11 12 13 14 15 16  14 15 16 17 18 19 20  12 13 14 15 16 17 18  ",
                    "17 18 19 20 21 22 23  21 22 23 24 25 26 27  19 20 21 22 23 24 25  ",
                    "24 25 26 27 28 29 30  28 29 30              26 27 28 29 30 31     ",
-                   "31                                                                "], year_pretty(complete_year(1886))
+                   "31                                                                "], cal.three_column_year_array
   end
   def test_18_print_year_is_correct
+    cal = Year.new(1886)
     assert_equal  """                            1886
       January               February               March          
 Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
@@ -268,49 +284,10 @@ Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
 17 18 19 20 21 22 23  21 22 23 24 25 26 27  19 20 21 22 23 24 25  
 24 25 26 27 28 29 30  28 29 30              26 27 28 29 30 31     
 31                                                                
-""", print_year(year_pretty(complete_year(1886)), 1886)
+""", cal.print_year
   end
   def test_19_generate_full_calendar_year
-    assert_equal  """                            1886
-      January               February               March          
-Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
-                1  2      1  2  3  4  5  6      1  2  3  4  5  6  
- 3  4  5  6  7  8  9   7  8  9 10 11 12 13   7  8  9 10 11 12 13  
-10 11 12 13 14 15 16  14 15 16 17 18 19 20  14 15 16 17 18 19 20  
-17 18 19 20 21 22 23  21 22 23 24 25 26 27  21 22 23 24 25 26 27  
-24 25 26 27 28 29 30  28                    28 29 30 31           
-31                                                                
-
-       April                  May                   June          
-Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
-             1  2  3                     1         1  2  3  4  5  
- 4  5  6  7  8  9 10   2  3  4  5  6  7  8   6  7  8  9 10 11 12  
-11 12 13 14 15 16 17   9 10 11 12 13 14 15  13 14 15 16 17 18 19  
-18 19 20 21 22 23 24  16 17 18 19 20 21 22  20 21 22 23 24 25 26  
-25 26 27 28 29 30     23 24 25 26 27 28 29  27 28 29 30           
-                      30 31                                       
-
-        July                 August              September        
-Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
-             1  2  3   1  2  3  4  5  6  7            1  2  3  4  
- 4  5  6  7  8  9 10   8  9 10 11 12 13 14   5  6  7  8  9 10 11  
-11 12 13 14 15 16 17  15 16 17 18 19 20 21  12 13 14 15 16 17 18  
-18 19 20 21 22 23 24  22 23 24 25 26 27 28  19 20 21 22 23 24 25  
-25 26 27 28 29 30 31  29 30 31              26 27 28 29 30        
-                                                                  
-
-      October               November              December        
-Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
-                1  2      1  2  3  4  5  6            1  2  3  4  
- 3  4  5  6  7  8  9   7  8  9 10 11 12 13   5  6  7  8  9 10 11  
-10 11 12 13 14 15 16  14 15 16 17 18 19 20  12 13 14 15 16 17 18  
-17 18 19 20 21 22 23  21 22 23 24 25 26 27  19 20 21 22 23 24 25  
-24 25 26 27 28 29 30  28 29 30              26 27 28 29 30 31     
-31                                                                
-""", generate_calendar_full_year(1886)
-  end
-
-  def test_20_generate_full_calendar_year
+    cal = Year.new(3000)
     assert_equal  """                            3000
       January               February               March          
 Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  
@@ -347,6 +324,6 @@ Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
 19 20 21 22 23 24 25  16 17 18 19 20 21 22  21 22 23 24 25 26 27  
 26 27 28 29 30 31     23 24 25 26 27 28 29  28 29 30 31           
                       30                                          
-""", generate_calendar_full_year(3000)
+""", cal.print_year
   end
 end
